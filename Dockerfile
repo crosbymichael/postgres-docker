fe676bd01c6a503315fbecf341d307fa955455d4
Michael Crosby <michael@crosbymichael.com>
2014-01-23 17:44:01 -0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu
+
+ENV PG_VERSION 9.1
+ENV USER docker
+ENV PASS SiHRDZ3Tt13uVVyH0ZST
+
+RUN apt-get -y install postgresql-$PG_VERSION postgresql-contrib-$PG_VERSION
+
+ADD postgresql.conf etc/postgresql/$PG_VERSION/main/postgresql.conf
+ADD pg_hba.conf etc/postgresql/$PG_VERSION/main/pg_hba.conf
+ADD start.sh start.sh
+
+EXPOSE 5432
+CMD ["/start.sh"]
---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+CONF="/etc/postgresql/${PG_VERSION}/main/postgresql.conf"
+DATA="/var/lib/postgresql/${PG_VERSION}/main"
+POSTGRES="/usr/lib/postgresql/${PG_VERSION}/bin/postgres"
+
+echo "Creating superuser: ${USER}"
+su postgres -c "${POSTGRES} --single -D ${DATA} -c config_file=${CONF}" <<EOF
+CREATE USER $USER WITH SUPERUSER PASSWORD '$PASS';
+EOF
+
+echo "Starting postgres:"
+su postgres -c "${POSTGRES} -D ${DATA} -c config_file=${CONF}"
